cfd438a4007d978c5692ff71ade8a24bd9ad3f0f
feliperromao <feliperomaocad@gmail.com>
2025-05-01 23:51:33 -0300

    feat: implement tests for product service

--- application/product_service.go
+++ application/product_service.go
@@ -8,8 +8,19 @@ func (s *ProductService) Get(id string) (ProductInterface, error) {
 	return s.ProductRepository.Get(id)
 }
 
-func (s *ProductService) Create(product ProductInterface) (ProductInterface, error) {
-	return s.ProductRepository.Create(product)
+func (s *ProductService) Create(name string, price float64) (ProductInterface, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Price = price
+	_, err := product.IsValid()
+	if err != nil {
+		return nil, err
+	}
+	result, err := s.ProductRepository.Create(product)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 func (s *ProductService) Enable(id string) (ProductInterface, error) {
@@ -19,6 +30,21 @@ func (s *ProductService) Enable(id string) (ProductInterface, error) {
 	}
 
 	err = product.Enable()
+	if err != nil {
+		return nil, err
+	}
+
+	return s.ProductRepository.Update(product)
+}
+
+
+func (s *ProductService) Disable(id string) (ProductInterface, error) {
+	product, err := s.ProductRepository.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	err = product.Disable()
 	if err != nil {
 		return nil, err
 	}
--- application/product_service_test.go
+++ application/product_service_test.go
@@ -25,3 +25,60 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	repository := mock_application.NewMockProductRepositoryInterface(ctrl)
+	repository.EXPECT().Create(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		ProductRepository: repository,
+	}
+
+	result, err := service.Create("Product Name", 100.0)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_Enable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil).AnyTimes()
+
+	repository := mock_application.NewMockProductRepositoryInterface(ctrl)
+	repository.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+	repository.EXPECT().Update(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		ProductRepository: repository,
+	}
+
+	result, err := service.Enable("123")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_Disable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(nil).AnyTimes()
+
+	repository := mock_application.NewMockProductRepositoryInterface(ctrl)
+	repository.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+	repository.EXPECT().Update(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		ProductRepository: repository,
+	}
+
+	result, err := service.Disable("123")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
